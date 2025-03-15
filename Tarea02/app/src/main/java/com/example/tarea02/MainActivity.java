b751b197b677e2b337b41fc8e1120a607d81651e
asahel <asahelmain@ciencias.unam.mx>
2025-03-15 00:52:01 -0600

    Cambia formato

--- Tarea02/app/src/main/java/com/example/tarea02/MainActivity.java
+++ Tarea02/app/src/main/java/com/example/tarea02/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.registro);
+        //setContentView(R.layout.registro);
+        setContentView(R.layout.catalogo);
     }
 }
